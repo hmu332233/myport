65bde5b949833220ca6720c797f7eafc766d52e9
minung han <hmu332233@gmail.com>
2017-03-22 13:03:45 +0000

    sign up design 적용

---
+++ app/assets/javascripts/devise.js
@@ -0,0 +1,5 @@
+//= require devise/jquery-2.2.4.min.js
+//= require devise/bootstrap.min.js
+//= require devise/jquery.bootstrap.wizard.js
+//= require devise/gsdk-bootstrap-wizard.js
+//= require devise/jquery.validate.min.js

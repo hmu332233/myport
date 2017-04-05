9485a0f8e3fbd42aa5faf4faa4c0d00d8a4147d5
minung han <hmu332233@gmail.com>
2017-04-05 09:10:35 +0000

    post-picture post-download_file 관계설정

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,37 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170321102827) do
+ActiveRecord::Schema.define(version: 20170405085643) do
+
+  create_table "download_files", force: :cascade do |t|
+    t.integer  "post_id"
+    t.string   "url"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "hash_tags", force: :cascade do |t|
+    t.integer  "post_id"
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "pictures", force: :cascade do |t|
+    t.integer  "post_id"
+    t.string   "url"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "posts", force: :cascade do |t|
+    t.string   "title"
+    t.string   "content"
+    t.string   "user_name"
+    t.integer  "scope"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false

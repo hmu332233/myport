1bc2f6bd025926be5542fec6e051366832b7ab7d
minung han <hmu332233@gmail.com>
2017-06-05 06:41:19 +0000

    멘토 멘티 모델링

--- app/controllers/blocks_controller.rb
+++ app/controllers/blocks_controller.rb
@@ -23,3 +23,28 @@ class BlocksController < ApplicationController
     
   end
 end
+class BlocksController < ApplicationController
+  
+  # POST /blocks
+  # parameter : user_id, block_user_id
+  def create
+    
+    _user_id = params[:user_id]
+    _block_user_id = params[:block_user_id]
+    
+    Block.create(user_id: _user_id, block_user_id: _block_user_id)
+    
+  end
+
+
+  # DELETE /blocks
+  # parameter : user_id, block_user_id
+  def delete
+    
+    _user_id = params[:user_id]
+    _block_user_id = params[:block_user_id]
+    
+    Block.where(user_id: _user_id, block_user_id: _block_user_id).first.delete
+    
+  end
+end

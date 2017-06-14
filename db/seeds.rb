dc5542fc644aa5bb9a25c395afb130bc8e4dadbf
minung han <hmu332233@gmail.com>
2017-06-14 08:17:45 +0000

    seed 추가, blank 사진 변경

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,3 +10,15 @@
 User.create(:name => "한민웅", :email => "test@test.com", :password => "123456")
 User.create(:name => "반상민", :email => "test2@test.com", :password => "123456")
 User.create(:name => "박도현", :email => "test3@test.com", :password => "123456")
+
+Post.create(title: "제목1", content: "내용1", scope: 0, user_name: "한민웅", user_id: 1)
+Post.create(title: "제목2", content: "내용2", scope: 0, user_name: "한민웅", user_id: 1)
+Post.create(title: "제목3", content: "내용3", scope: 0, user_name: "한민웅", user_id: 1)
+
+Post.create(title: "제목1", content: "내용1", scope: 0, user_name: "반상민", user_id: 2)
+Post.create(title: "제목2", content: "내용2", scope: 0, user_name: "반상민", user_id: 2)
+Post.create(title: "제목3", content: "내용3", scope: 0, user_name: "반상민", user_id: 2)
+
+Post.create(title: "제목1", content: "내용1", scope: 0, user_name: "박도현", user_id: 3)
+Post.create(title: "제목2", content: "내용2", scope: 0, user_name: "박도현", user_id: 3)
+Post.create(title: "제목3", content: "내용3", scope: 0, user_name: "박도현", user_id: 3)
